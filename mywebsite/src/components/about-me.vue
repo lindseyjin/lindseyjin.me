a4a1a94224565fea3b7869d53c0af30e88f79c4d
Lindsey Jin <l44jin@uwaterloo.ca>
2018-08-12 17:51:17 -0400

    initial commit

---
+++ mywebsite/src/components/about-me.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+  <h1>About Me</h1>
+    <body>  Hi, my name is Lindsey Jin. I'm currently studying Software Engineering at the University of Waterloo.
+    write a lot more aaaaaaaaaaaaaaaaaaaaaaaaa
+    asdf.kkahshkfjhaekej akejfahksjdfhksj
+    </body>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AboutMe',
+    data () {
+      return {}
+    }
+  }
+</script>
+<style>
+  .app-container {
+    width: 70%;
+    margin: auto;
+  }
+</style>
