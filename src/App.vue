d14246474ec2e5df9332b263878a4876ee56cd2b
Lindsey Jin <l44jin@uwaterloo.ca>
2019-02-01 22:37:49 -0500

    restructuring folder

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="no-margin">
+    <Header></Header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Header from './components/header'
+  export default {
+    name: 'App',
+    components: {Header}
+  }
+</script>
+
+<style>
+  #header-image {
+    height: 400px;
+    width: 100%;
+  }
+  body {
+    height: 100%;
+    margin: 0;
+  }
+</style>
